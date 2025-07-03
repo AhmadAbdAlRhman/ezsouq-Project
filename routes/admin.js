7afb8b178aa798e828b9da9ef199486762e3f443
AhmadAbdAlRhman <ahmadhajnajeeb45@gmail.com>
2025-07-03 16:57:11 +0300

    4

--- routes/admin.js
+++ routes/admin.js
@@ -4,27 +4,28 @@ const checkRole = require('../middleware/checkRole');
 const protect = require('../middleware/OAtuh');
 const Governorates = require('../controllers/admin/Governorates');
 const Users = require('../controllers/admin/users');
-
+//إضافة محافظة مع المدن التابعة لها
 router.post('/add_governorates',
     protect,
     checkRole(['ADMIN','OWNER']),
     Governorates.addGovernorates);
-//=>update Governorates and cities which belong to it
+//=>تعديل أسماء المحافظة والمدن التابعة لها
 router.put('/update_governorate/:gov_id',
     protect,
     checkRole(['ADMIN', 'OWNER']),
     Governorates.updateGovernorate
 );
+//منح صلاحية الأدمن للمستخدم العادي
 router.put('/Granting_permissions', 
     protect,
     checkRole(['OWNER']),
     Users.GrantingPermissions);
-
+//سحب صلاحية الأدمن
 router.put('/Withdraw_permissions', 
     protect,
     checkRole(['OWNER']),
     Users.WithdrawPermissions);
-
+//owner حذف المستخدم العادي والأدمن من قبل ال  
 router.delete('/Delet_User', 
     protect,
     checkRole(['OWNER']),
