1e1c4cb0f04e4d009dc94391447567ef87b38696
AhmadAbdAlRhman <ahmadhajnajeeb45@gmail.com>
2025-07-03 19:33:28 +0300

    add category

---
+++ controllers/admin/Products.js
@@ -0,0 +1,30 @@
+const Category = require('../../models/Category');
+
+module.exports.addCtegory = async (req, res) => {
+    try {
+        const name = req.body.name;
+        if (!name)
+            return res.status(400).json({
+                message: "..الاسم مطلوب"
+            });
+        const existing = await Category.findOne({
+            name
+        });
+        if (existing)
+            return res.status(409).json({
+                message: "التصنيف موجود مسبقاً"
+            });
+        const newCategory = new Category({
+            name
+        });
+        await newCategory.save();
+        return res.status(201).json({
+            message: "تمت إضافة التصنيف بنجاح"
+        });
+    } catch (err) {
+        res.status(500).json({
+            message: "حدث خطأ في السيرفر",
+            error: error.message
+        });
+    }
+}
--- routes/admin.js
+++ routes/admin.js
@@ -3,11 +3,12 @@ const router = express.Router();
 const checkRole = require('../middleware/checkRole');
 const protect = require('../middleware/OAtuh');
 const Governorates = require('../controllers/admin/Governorates');
+const products = require('../controllers/admin/Products');
 const Users = require('../controllers/admin/users');
 //إضافة محافظة مع المدن التابعة لها
 router.post('/add_governorates',
     protect,
-    checkRole(['ADMIN','OWNER']),
+    checkRole(['ADMIN', 'OWNER']),
     Governorates.addGovernorates);
 //=>تعديل أسماء المحافظة والمدن التابعة لها
 router.put('/update_governorate/:gov_id',
@@ -16,18 +17,24 @@ router.put('/update_governorate/:gov_id',
     Governorates.updateGovernorate
 );
 //منح صلاحية الأدمن للمستخدم العادي
-router.put('/Granting_permissions', 
+router.put('/Granting_permissions',
     protect,
     checkRole(['OWNER']),
     Users.GrantingPermissions);
 //سحب صلاحية الأدمن
-router.put('/Withdraw_permissions', 
+router.put('/Withdraw_permissions',
     protect,
     checkRole(['OWNER']),
     Users.WithdrawPermissions);
-//owner حذف المستخدم العادي والأدمن من قبل ال  
-router.delete('/Delet_User', 
+//owner حذف المستخدم العادي والأدمن من قبل ال
+router.delete('/Delet_User',
     protect,
     checkRole(['OWNER']),
     Users.DeleteUser);
+//إضافة التنصيف
+router.post('/add_category',
+    protect,
+    checkRole(['ADMIN', 'OWNER']),
+    products.addCtegory);
+
 module.exports = router;
