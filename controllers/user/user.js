0d8d2fc55142d62baa31d96068f27d2491f7c741
AhmadAbdAlRhman <ahmadhajnajeeb45@gmail.com>
2025-07-08 14:45:47 +0300

    get One User

--- controllers/user/user.js
+++ controllers/user/user.js
@@ -1,5 +1,21 @@
 const User = require('../../models/users');
+const mongoose = require('mongoose');
 
-module.exports.getOneUser = async (req, res) =>{
-
+module.exports.getOneUser = async (req, res) => {
+    const user_id = req.params.user_id;
+    if (!mongoose.Types.ObjectId.isValid(user_id))
+        return res.status(400).json({
+            message: "معرّف غير صالح"
+        });
+    await User.findById(user_id).select('_id name email Role').then((user) => {
+        if (!user)
+            return res.status(404).json({message:"لا يوجد مثل هذا المستخدم"});
+        return res.status(200).json(user);
+    }).catch((err) => {
+        res.status(500)
+            .json({
+                message: "Error Server",
+                Error: err.details
+            });
+    });
 }
--- routes/user.js
+++ routes/user.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const governorates = require('../controllers/user/Governorates');
 const products = require('../controllers/user/products');
+const user = require('../controllers/user/user');
 const upload = require('../middleware/upload_files');
 const protect = require('../middleware/OAtuh');
 
@@ -13,6 +14,8 @@ router.get('/sortedProducts', products.getAllSortedProducts);
 router.get('/fliteredProducts', products.getFilteredProducts);
 //-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_
 router.get('/product/:id', products.getOneProduct);
+//-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_
+router.get('/user/:user_id', user.getOneUser);
 //-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_
 router.post('/add_product',
     protect,
