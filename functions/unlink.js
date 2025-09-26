ae368f1368f897c0df15369d2ee1bc3f25f6d069
AhmadAbdAlRhman <ahmadhajnajeeb45@gmail.com>
2025-09-27 01:33:03 +0300

    promises

--- functions/unlink.js
+++ functions/unlink.js
@@ -1,10 +1,13 @@
-const fs = require('fs');
+const fs = require('fs').promises;
+
 async function safeUnlink(filePath) {
     try {
         await fs.unlink(filePath);
         console.log("✅ تم حذف الملف:", filePath);
     } catch (err) {
-        console.error("❌ فشل حذف الملف:", err);
+        if (err.code !== 'ENOENT') { // تجاهل إذا الملف مش موجود
+            console.error("❌ فشل حذف الملف:", err.message);
+        }
     }
 }
 
